46d8b6ec4b24b4c9c4154092abaaf83c8c9755e3
Kamapra18 <marioprayoga18@gmail.com>
2025-05-08 11:15:05 +0700

    perubahan script js

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-let speech = new SpeechSynthesisUtterance();
+const speech = new SpeechSynthesisUtterance();
 let voices = [];
+const voiceSelect = document.querySelector("select");
 
-let voiceSelect = document.querySelector("select");
-window.speechSynthesis.onvoiceschanged = () => {
+function populateVoices() {
   voices = window.speechSynthesis.getVoices();
+  voiceSelect.innerHTML = ""; 
+
   voices.forEach((voice) => {
-    let option = document.createElement("option");
+    const option = document.createElement("option");
     option.value = voice.name;
-    option.innerText = voice.name;
+    option.innerText = `${voice.name} (${voice.lang})`;
     voiceSelect.appendChild(option);
   });
-};
+
+  if (voices.length > 0 && !speech.voice) {
+    speech.voice = voices[0];
+    voiceSelect.value = voices[0].name;
+  }
+}
+
+window.speechSynthesis.onvoiceschanged = populateVoices;
+populateVoices(); 
 
 voiceSelect.addEventListener("change", () => {
   speech.voice = voices.find((voice) => voice.name === voiceSelect.value);
 });
 
+
 document.querySelector("button").addEventListener("click", () => {
-  speech.text = document.querySelector("textarea").value;
-  window.speechSynthesis.speak(speech);
+  const text = document.querySelector("textarea").value.trim();
+  if (text) {
+    speech.text = text;
+    window.speechSynthesis.cancel(); 
+    window.speechSynthesis.speak(speech);
+  } else {
+    alert("Silakan masukkan teks terlebih dahulu.");
+  }
 });
